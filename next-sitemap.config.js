d46631cd8b49bc4b47a2bf5ed5da46c8dff93306
Amit Yadav <vy9172631@gmail.com>
2025-04-16 11:52:40 +0530

    created xml file

---
+++ next-sitemap.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://beingxbot.tech',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  autoLastmod: true,
+  exclude: ['/404', '/server-sitemap.xml'],
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: 'daily',
+      priority: 0.7,
+    };
+  },
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://beingxbot.tech/sitemap-0.xml',
+      'https://beingxbot.tech/server-sitemap.xml',
+    ],
+  },
+};
